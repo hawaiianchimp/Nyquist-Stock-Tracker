fac1a869c957c829f57d0d51aad87cc4714c841d
Sean Burke <hawaiianchimp@gmail.com>
2016-01-16 17:32:15 -0800

    added exponential moving average function

--- app/services/math-service.js
+++ app/services/math-service.js
@@ -10,27 +10,47 @@ export default Ember.Service.extend({
    * @param  {Array} data  [The array of values]
    * @return {Number}       The simple moving average
    */
-  movingAverage(limit, data) {
-    let values = this.get('inputService').convertToArray(data);
-    let total = 0;
-    let days = parseFloat(limit);
-    let sma;
-    if (Ember.isEmpty(values) || Ember.isEmpty(days) || values.length === 0 || days === 0){
+  sma(limit, data) {
+    Ember.assert('Limit must be an Integer', typeof limit === 'number');
+    Ember.assert('Data must be an Array', data.constructor === Array);
+
+    if (limit <= 0 || data.length === 0) {
       return 0;
     }
-
-    if (data.length > days) {
-      values = values.splice(-days);
-    }
-
-    for (var i = values.length - 1; i >= 0; i--) {
-      total += parseFloat(values[i]);
-    }
-    sma = total/values.length;
-    return this.roundToNearest(4, sma);
+    return data.splice(-limit).reduce((prev, curr, index, values) => {
+      return prev + curr/values.length;
+    }, 0);
   },
 
   /**
+   * Recursive function to calculate Exponential Moving Average
+   *
+   * @param {Number} limit [The number of data points to calculate over]
+   * @param {Array} data [The data]
+   * @returns {Number}
+   */
+  ema: function ema(limit, data) {
+    Ember.assert('Limit must be an Integer', typeof limit === 'number');
+    Ember.assert('Data must be an Array', data.constructor === Array);
+
+    if (limit <= 0 || data.length === 0) {
+      return 0;
+    }
+    if(limit === 1 || data.length === 1) {
+      return data[0];
+    }
+    else {
+      let values = data.slice(-limit);
+      let prev = values.slice(0,-1);
+      let curr = values.slice(-1);
+      let weight = Math.round(2/(values.length + 1) * 1000000) / 1000000;
+      let result = weight * curr[0] + (1 - weight) * ema(prev.length, prev);
+      return Math.round(result * 100) / 100;
+    }
+  },
+
+  /**
+   * Rounds number to the specified number of digits
    *
    * @param  {Number} decimal Digits to round number to
    * @param  {Number} number  Number to round
--- tests/unit/services/math-service-test.js
+++ tests/unit/services/math-service-test.js
@@ -10,25 +10,72 @@ moduleFor('service:math-service', 'Unit | Service | math service', {
 	}
 });
 
-test('movingAverage', function(assert) {
-	assert.expect(18);
-	assert.equal(service.movingAverage(0, [1,10,1]), 0, 'testing zero days selected');
-	assert.equal(service.movingAverage(3, []), 0, 'testing empty array');
-	assert.equal(service.movingAverage(0, undefined), 0, 'testing undefined input');
-	assert.equal(service.movingAverage(200, '1,2,3,4,5'), 3, 'testing days greater than array.length');
-	assert.equal(service.movingAverage('200', '1,2,3,4,5'), 3, 'testing string inputs');
-	assert.equal(service.movingAverage('200', [1,2,3,4,5]), 3, 'testing array inputs');
-	assert.equal(service.movingAverage('2', 'cheeseburger'), 0, 'testing nonsense string input');
-	assert.equal(service.movingAverage(3, 1), 1, 'testing single number input');
-	assert.equal(service.movingAverage(3, [1,1,1]), 1, 'testing days equal to array.length');
-	assert.equal(service.movingAverage(2, [1,1,1]), 1, 'testing days less than array.length');
-	assert.equal(service.movingAverage(4, [1,1,1]), 1, 'testing days greating than array.length');
-	assert.equal(service.movingAverage(3, [2,5,2]), 3, 'testing different numbers');
-	assert.equal(service.movingAverage(3, [2,4,7,4]), 5, 'testing larger arrays');
-	assert.equal(service.movingAverage(5, [2,4,728,4,10,2,6]), 150, 'testing larger inputs');
-	assert.equal(service.movingAverage(200, [1,2,3,4,5]), 3, 'testing large days selection');
-	assert.equal(service.movingAverage(200, [1,2,3,4,6,2,63,5]), 10.75, 'testing decimal output');
-	assert.equal(service.movingAverage(200, [1.45,2.25,3.12,4.76,6.2,2.3,63.9,5.6]), 11.1975, 'testing decimal input');
-	assert.equal(service.movingAverage(3, [4,5,2]), 3.6667, 'testing rounding of repeating decimal output');
+test('Simple Moving Average', function(assert) {
+	assert.expect(13);
+	assert.equal(service.sma(0, [1,10,1]), 0, 'testing zero days selected');
+	assert.equal(service.sma(3, []), 0, 'testing empty array');
+	assert.equal(service.sma(3, [1,1,1]), 1, 'testing days equal to array.length');
+	assert.equal(service.sma(2, [1,1,1]), 1, 'testing days less than array.length');
+	assert.equal(service.sma(4, [1,1,1]), 1, 'testing days greating than array.length');
+	assert.equal(service.sma(3, [2,5,2]), 3, 'testing different numbers');
+	assert.equal(service.sma(3, [2,4,7,4]), 5, 'testing larger arrays');
+	assert.equal(service.sma(5, [2,4,728,4,10,2,6]), 150, 'testing larger inputs');
+	assert.equal(service.sma(200, [1,2,3,4,5]), 3, 'testing large days selection');
+  assert.equal(service.sma(10.9, [1,2,3,4,5]), 3, 'Function does not break on decimal limit');
+	assert.equal(service.sma(200, [1,2,3,4,6,2,63,5]), 10.75, 'testing decimal output');
+	assert.equal(service.sma(200, [1.45,2.25,3.12,4.76,6.2,2.3,63.9,5.6]), 11.1975, 'testing decimal input');
+	assert.equal(service.sma(3, [4,5,2]), (4+5+2)/(3), 'testing rounding of repeating decimal output');
 });
 
+test('Exponential Moving Average', function(assert) {
+	assert.expect(17);
+	assert.equal(service.ema(0, [1,10,1]), 0, 'testing zero days selected');
+	assert.equal(service.ema(3, []), 0, 'testing empty array');
+	assert.equal(service.ema(3, [1,1,1]), 1, 'testing days equal to array.length');
+	assert.equal(service.ema(2, [1,1,1]), 1, 'testing days less than array.length');
+	assert.equal(service.ema(4, [1,1,1]), 1, 'testing days greating than array.length');
+	assert.equal(service.ema(3, [2,5,2]), 3, 'testing different numbers');
+	assert.equal(service.ema(3, [2,4,7,4]), 5, 'testing larger arrays');
+	assert.equal(service.ema(5, [2,4,728,4,10,2,6]), 53.6, 'testing larger inputs');
+  assert.equal(service.ema(10, [1,2,3,4,5,6,7,8,9,10]), 7.01, 'testing larger inputs');
+  assert.equal(service.ema(10, [10,9,8,7,6,5,4,3,2,1]), 3.99, 'testing larger inputs');
+	assert.equal(service.ema(5, [2,4,6,4,10,3,728]), 246.4, 'testing larger inputs');
+	assert.equal(service.ema(200, [1,2,3,4,5]), 3.67, 'testing large days selection');
+	assert.equal(service.ema(200, [1,2,3,4,6,2,63,5]), 15.36, 'testing decimal output');
+	assert.equal(service.ema(200, [1.45,2.25,3.12,4.76,6.2,2.3,63.9,5.6]), 15.86, 'testing decimal input');
+	assert.equal(service.ema(3, [4,5,2]), 3.34, 'testing rounding of repeating decimal output');
+  assert.equal(service.ema(10, [22.27,22.19,22.08,22.17,22.18,22.13,22.23,22.43,22.24,22.29]), 22.25, 'testing rounding of repeating decimal output');
+  assert.equal(service.ema(10, [
+    22.27,
+    22.19,
+    22.08,
+    22.17,
+    22.18,
+    22.13,
+    22.23,
+    22.43,
+    22.24,
+    22.29,
+    22.15,
+    22.39,
+    22.38,
+    22.61,
+    23.36,
+    24.05,
+    23.75,
+    23.83,
+    23.95,
+    23.63,
+    23.82,
+    23.87,
+    23.65,
+    23.19,
+    23.10,
+    23.33,
+    23.68,
+    23.10,
+    22.40,
+    22.17
+    ]),
+    23, 'testing rounding of repeating decimal output');
+});
